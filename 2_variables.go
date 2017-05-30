a5c4e53570c4c4affed99e9c051450f7ea554675
Ismael Haytam <anonismaa@gmail.com>
2017-05-30 02:37:57 +0200

    Golang - Conceptos básicos

---
+++ 2_variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    nombre := "Ismael"
+
+    nombre = "Haytam"
+
+    fmt.Println(nombre)
+}
---
+++ 4_output.go
@@ -0,0 +1,12 @@
+package main
+
+import(
+    "fmt"
+)
+
+func main() {
+    edad := 21
+    nombre := "Ismael Haytam"
+    macho_alfa := true
+    fmt.Printf("Edad %d nombre %v macho %t", edad, nombre, macho_alfa)
+}
