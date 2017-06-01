3b9f633a3fe291ffc47f2b58093c837ec2432211
Ismael Haytam <anonismaa@gmail.com>
2017-06-01 23:35:12 +0200

    Golang - Nivel intermedio

--- 17_goroutines.go
+++ 17_goroutines.go
@@ -2,17 +2,23 @@ package main
 
 import (
     "fmt"
+    "time"
     "strings"
 )
 
 func main() {
-    mi_nombre_lento("Ismael Haytam")
+    go mi_nombre_lento("Ismael Haytam")
+    fmt.Println("Que Aburrimiento...")
+
+    var wait string
+    fmt.Scanln(&wait)
 }
 
 func mi_nombre_lento(name string)  {
     letras := strings.Split(name, "")
 
     for _,letra := range(letras) {
+        time.Sleep(500 * time.Millisecond)
         fmt.Println(letra)
     }
 }
