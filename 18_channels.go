3bccc0afe42135920e07e14e406c2f8a96eed3bd
Ismael Haytam <anonismaa@gmail.com>
2017-06-01 23:49:41 +0200

    Golang - Nivel intermedio

--- 18_channels.go
+++ 18_channels.go
@@ -1,5 +1,21 @@
 package main
 
-func main()  {
+import "fmt"
 
+func main()  {
+    channel := make(chan string)
+
+    go func(channel chan string) {
+        for {
+            var name string
+            fmt.Scanln(&name)
+            channel <- name
+        }
+    }(channel)
+
+    msg := <- channel
+    fmt.Println("Test del canal ... " + msg)
+
+    msg = <- channel
+    fmt.Println("Test del canal ... " + msg)
 }
