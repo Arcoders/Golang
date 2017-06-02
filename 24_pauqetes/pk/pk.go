7076214783e035676507ff8795186ca0a11fcb2f
Ismael Haytam <anonismaa@gmail.com>
2017-06-02 06:17:03 +0200

    Golang - Nivel intermedio

---
+++ 24_pauqetes/pk/pk.go
@@ -0,0 +1,11 @@
+package pk
+
+var defaults string
+
+func init() {
+    defaults = "Arcoders..."
+}
+
+func TestPk() string{
+    return defaults
+}
---
+++ 24_pauqetes/principal.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+    "fmt"
+    "./pk"
+)
+
+func main() {
+    fmt.Println(pk.TestPk())
+}
