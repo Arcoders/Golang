760b6382ea961d547dc0c21041113db664de7d1e
Ismael Haytam <anonismaa@gmail.com>
2017-06-03 23:30:20 +0200

    Golang - Conceptos básicos

---
+++ 26_repaso/maps.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    x := make(map[string]string, 2)
+    fmt.Println(x)
+
+    fmt.Println("--------------------------------")
+
+    x["nombre"] = "Ismael"
+    x["edad"] = "21"
+
+    fmt.Println(x)
+
+    fmt.Println("Mi nombre: " + x["nombre"])
+    fmt.Println("Tengo: " + x["edad"])
+
+    fmt.Println("--------------------------------")
+
+    alfnum := map[string]int{
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": 4,
+        "e": 6,
+    }
+
+    fmt.Println(alfnum["d"])
+    alfnum["e"]--
+    fmt.Println(alfnum["e"])
+
+    fmt.Println("--------------------------------")
+
+    dia := map[int]string{
+        1: "Lunes",
+        2: "Martes",
+        3: "Miercoles",
+        4: "Jueves",
+        5: "Abril",
+    }
+
+    fmt.Println(dia[2])
+
+    fmt.Println("--------------------------------")
+
+    delete(dia, 5)
+    fmt.Println(dia)
+
+    fmt.Println("--------------------------------")
+
+    if dia, ok := dia[5]; ok {
+        if dia == "Miercoles" {
+            fmt.Println("Correcto el dia 3 es el " + dia)
+        } else {
+            fmt.Println("Día incorrecto")
+        }
+    } else {
+        fmt.Println("El valor no existe")
+    }
+}
