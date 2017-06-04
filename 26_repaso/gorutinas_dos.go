963980794afaf6e757f8f0ffe552ba1a72b762e5
Ismael Haytam <anonismaa@gmail.com>
2017-06-05 01:15:11 +0200

    Golang - Conceptos básicos

---
+++ 26_repaso/gorutinas_dos.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    go animacion(100 * time.Millisecond)
+    const n = 40
+
+    resultado := fib(n)
+    fmt.Printf("Fibonacci(%d) = %d \n", n ,resultado)
+}
+
+func animacion(retraso time.Duration) {
+    for {
+        for _, r := range `\|/-` {
+            fmt.Printf("\r %c", r)
+            time.Sleep(retraso)
+        }
+    }
+}
+
+func fib(x int) int {
+    if x < 2 {
+        return x
+    }
+    return fib(x-1) + fib(x-2)
+}
