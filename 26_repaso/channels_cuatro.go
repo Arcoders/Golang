021c9b303a7c38c3e9e988794995e1e335869219
Ismael Haytam <anonismaa@gmail.com>
2017-06-05 20:58:16 +0200

    Golang - Conceptos básicos

---
+++ 26_repaso/channels_cuatro.go
@@ -0,0 +1,30 @@
+package main
+
+import(
+    "fmt"
+    "time"
+)
+
+func main() {
+    numero := make(chan int)
+    cuadrado := make(chan int)
+
+    go func() {
+        for x := 0; x < 5; x++ {
+            numero <- x
+        }
+        close(numero)
+    }()
+
+    go func() {
+        for x := range numero {
+            cuadrado <- x * x
+        }
+        close(cuadrado)
+    }()
+
+    for x := range cuadrado {
+        fmt.Println(x)
+        time.Sleep(1 * time.Second)
+    }
+}
