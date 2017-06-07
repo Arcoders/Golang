41947c8fd5dd6a1d88148d8f57c9ae6920f60b17
Ismael Haytam <anonismaa@gmail.com>
2017-06-07 21:23:38 +0200

    Golang - Conceptos básicos

--- 26_repaso/chatserver.go
+++ 26_repaso/chatserver.go
@@ -9,4 +9,18 @@ import (
 
 func main() {
     conn, err := net.Dial("tcp", "localhost:8005")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    done := make(chan struct{})
+
+    go func() {
+        io.Copy(os.Stdout, conn)
+        log.Println("Terminamos")
+        done <- struct{}{}
+    }()
+
+    mustCopy(Conn, os.Stdin)
+    conn.Close()
 }
