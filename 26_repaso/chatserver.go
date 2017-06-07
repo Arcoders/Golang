3d4d8f3de9895b583c8a9997368469e4c154fbcc
Ismael Haytam <anonismaa@gmail.com>
2017-06-07 23:56:57 +0200

    Golang - chat TCP

--- 26_repaso/chatserver.go
+++ 26_repaso/chatserver.go
@@ -1,26 +1,79 @@
 package main
 
 import (
-    "io"
+    "bufio"
+    "fmt"
     "log"
     "net"
-    "os"
 )
 
-func main() {
-    conn, err := net.Dial("tcp", "localhost:8005")
-    if err != nil {
-        log.Fatal(err)
-    }
+type client chan<- string
 
-    done := make(chan struct{})
+var (
+entrantes = make(chan client)
+salientes = make(chan client)
+mensajes = make(chan string)
+)
 
-    go func() {
-        io.Copy(os.Stdout, conn)
-        log.Println("Terminamos")
-        done <- struct{}{}
-    }()
+func broadcaster() {
+clients := make(map[client]bool)
+for {
+	select {
+	case msg := <- mensajes:
+		for cli := range clients {
+			cli <- msg
+		}
 
-    mustCopy(Conn, os.Stdin)
-    conn.Close()
+		case cli := <- entrantes:
+			clients[cli] = true
+
+			case cli := <-salientes:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
 }
+
+func handleConn(conn net.Conn) {
+	ch := make(chan string)
+	go clientWriter(conn, ch)
+
+	quien := conn.RemoteAddr().String()
+	ch <- "Tú eres" + quien
+
+	mensajes <- quien + "se ha conectado a la sala"
+	entrantes <- ch
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		mensajes <- quien + ":" + input.Text()
+	}
+
+	salientes <- ch
+	mensajes <- quien + "se ha ido"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch{
+		fmt.Fprintln(conn, msg)
+	}
+}
+
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8009")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+		}
+	}
